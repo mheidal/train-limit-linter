23b300f3dce6dd3e5f00a77828c521c5134074c8
Mike Heidal <mikeheidal@gmail.com>
2023-10-01 15:50:28 -0700

    Added constants file

---
+++ constants.lua
@@ -0,0 +1,20 @@
+local constants = {}
+
+constants.actions = {
+    train_schedule_create_blueprint="train_schedule_create_blueprint",
+    toggle_excluded_keyword="toggle_excluded_keyword",
+    delete_excluded_keyword="delete_excluded_keyword",
+    train_report_update="train_report_update",
+    select_fuel="select_fuel",
+    close_window="close_window",
+    exclude_textfield_apply="exclude_textfield_apply",
+    delete_all_excluded_strings="delete_all_excluded_strings",
+    hide_textfield_apply="hide_textfield_apply",
+    delete_hidden_keyword="delete_hidden_keyword",
+    delete_all_hidden_keywords="delete_all_hidden_keywords",
+    toggle_current_surface="toggle_current_surface",
+    toggle_show_satisfied="toggle_show_satisfied",
+    toggle_show_invalid="toggle_show_invalid"
+}
+
+return constants
