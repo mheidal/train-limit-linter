e8f0bf582dc7af30e9408fb94270633e95f616bb
Mike Heidal <mikeheidal@gmail.com>
2023-10-05 13:42:22 -0700

    Changed description of orientation selector

--- views/blueprint_orientation_selector.lua
+++ views/blueprint_orientation_selector.lua
@@ -5,27 +5,36 @@ Exports = {}
 local function build_blueprint_orientation_selector(selected_orientation, parent)
 
     local direction_selector_flow = parent.add{type="flow", direction="vertical"}
-    direction_selector_flow.add{type="label", caption="Set train direction in blueprints."}
+    direction_selector_flow.add{type="label", caption={"tll.set_direction"}, tooltip={"tll.set_direction_tooltip"}}
+    local selected_direction_label = direction_selector_flow.add{type="label"}
+
     local direction_table = direction_selector_flow.add{type="table", column_count=3}
 
     local orientations = constants.orientations
     local ordered_orientations = {
-        orientations.ul,
-        orientations.u,
-        orientations.ur,
-        orientations.l,
+        {"tll.orientation_northwest", orientations.ul},
+        {"tll.orientation_north", orientations.u},
+        {"tll.orientation_northeast", orientations.ur},
+        {"tll.orientation_west", orientations.l},
         -1,
-        orientations.r,
-        orientations.dl,
-        orientations.d,
-        orientations.dr
+        {"tll.orientation_east", orientations.r},
+        {"tll.orientation_southwest", orientations.dl},
+        {"tll.orientation_south", orientations.d},
+        {"tll.orientation_southeast", orientations.dr},
     }
 
-    for _, orientation in pairs(ordered_orientations) do
-        if orientation == -1 then
+    for _, name_orientation_tuple in pairs(ordered_orientations) do
+        if name_orientation_tuple == -1 then
             direction_table.add{type="empty-widget"}
         else
-            local button_style = (orientation == selected_orientation) and "yellow_slot_button" or "recipe_slot_button"
+            local caption, orientation = table.unpack(name_orientation_tuple)
+            local button_style
+            if orientation == selected_orientation then
+                selected_direction_label.caption = {"", {"tll.selected_direction"}, " ", {caption}, "."}
+                button_style = "yellow_slot_button"
+            else
+                button_style = "recipe_slot_button"
+            end
             direction_table.add{type="sprite-button", style=button_style, tags={action=constants.actions.set_blueprint_orientation, orientation=orientation}}
         end
     end
