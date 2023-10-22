b1dbc33525f2f16d10fe652849c6810c68ec72fb
Mike Heidal <mikeheidal@gmail.com>
2023-10-21 20:42:25 -0700

    Initial manual mode display, pending graphics change

--- models/schedule_table_configuration.lua
+++ models/schedule_table_configuration.lua
@@ -2,10 +2,12 @@
 ---@field only_current_surface boolean
 ---@field show_satisfied boolean
 ---@field show_invalid boolean
+---@field show_manual boolean
 ---@field new fun(): TLLScheduleTableConfiguration
 ---@field toggle_current_surface fun(self: TLLScheduleTableConfiguration)
 ---@field toggle_show_satisfied fun(self: TLLScheduleTableConfiguration)
 ---@field toggle_show_invalid fun(self: TLLScheduleTableConfiguration)
+---@field toggle_show_manual fun(self: TLLScheduleTableConfiguration)
 
 ---@class TLLScheduleTableConfiguration
 local TLLScheduleTableConfiguration = {}
@@ -17,6 +19,7 @@ function TLLScheduleTableConfiguration.new()
         only_current_surface = true,
         show_satisfied = true, -- satisfied when sum of train limits is 1 greater than sum of trains
         show_invalid = false, -- invalid when train limits are not set for all stations in name group
+        show_manual = false,
     }
     setmetatable(self, mt)
     return self
@@ -32,6 +35,10 @@ end
 
 function TLLScheduleTableConfiguration:toggle_show_invalid()
     self.show_invalid = not self.show_invalid
+end
+
+function TLLScheduleTableConfiguration:toggle_show_manual()
+    self.show_manual = not self.show_manual
 end
 
 return TLLScheduleTableConfiguration
