0be15550ed9b5495d7e44836978624c01dc201a6
Mike Heidal <mikeheidal@gmail.com>
2023-11-06 13:32:44 -0800

    Added show/hide to display settings.

--- constants.lua
+++ constants.lua
@@ -23,6 +23,7 @@ constants.actions = {
     toggle_show_not_set="toggle_show_not_set",
     toggle_show_manual="toggle_show_manual",
     toggle_show_dynamic="toggle_show_dynamic",
+    toggle_show_settings="toggle_show_settings",
 
     icon_selector_textfield_apply="icon_selector_textfield_apply",
     icon_selector_icon_selected="icon_selector_icon_selected",
