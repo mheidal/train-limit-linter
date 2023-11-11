db7586261d87b16a06c5060b64224d56ee466a05
Mike Heidal <mikeheidal@gmail.com>
2023-11-11 15:17:03 -0800

    Revert "Revert "Copy train stops""
    This reverts commit c691c71074757a05549cb8c4fc6df5d693c8a5b9.

--- models/blueprint_configuration.lua
+++ models/blueprint_configuration.lua
@@ -3,14 +3,20 @@ local constants = require("constants")
 ---@class TLLBlueprintConfiguration
 ---@field new_blueprint_orientation number -- add type hints to constants?
 ---@field snap_enabled boolean
----@field snap_direction string,
+---@field snap_direction string
 ---@field snap_width number
+---@field include_train_stops boolean
+---@field limit_train_stops boolean
+---@field default_train_limit number
 ---@field new fun(): TLLBlueprintConfiguration
 ---@field set_new_blueprint_orientation fun(self: TLLBlueprintConfiguration, new_orientation: number)
 ---@field set_snap_width fun(self: TLLBlueprintConfiguration, new_snap_width: number)
 ---@field toggle_snap_direction fun(self: TLLBlueprintConfiguration)
 ---@field set_snap_direction fun(self: TLLBlueprintConfiguration, new_snap_direction: string)
 ---@field toggle_blueprint_snap fun(self: TLLBlueprintConfiguration)
+---@field toggle_include_train_stops fun(self: TLLBlueprintConfiguration)
+---@field toggle_limit_train_stops fun(self: TLLBlueprintConfiguration)
+---@field set_default_train_limit fun(self: TLLBlueprintConfiguration, limit: number)
 
 local TLLBlueprintConfiguration = {}
 local mt = { __index = TLLBlueprintConfiguration }
@@ -21,7 +27,10 @@ function TLLBlueprintConfiguration.new()
         new_blueprint_orientation = constants.orientations.d,
         snap_enabled = true,
         snap_direction = constants.snap_directions.horizontal,
-        snap_width = 2
+        snap_width = 2,
+        include_train_stops=false,
+        limit_train_stops=false,
+        default_train_limit=0,
     }
     setmetatable(self, mt)
     return self
@@ -48,6 +57,19 @@ end
 
 function TLLBlueprintConfiguration:toggle_blueprint_snap()
     self.snap_enabled = not self.snap_enabled
+end
+
+function TLLBlueprintConfiguration:toggle_include_train_stops()
+    self.include_train_stops = not self.include_train_stops
+end
+
+function TLLBlueprintConfiguration:toggle_limit_train_stops()
+    self.limit_train_stops = not self.limit_train_stops
+
+end
+
+function TLLBlueprintConfiguration:set_default_train_limit(limit)
+    self.default_train_limit = limit
 end
 
 return TLLBlueprintConfiguration
